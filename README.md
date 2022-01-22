38951aaf208ebf3757509aa6e9873b3538567b5b
Zsvob <svobodazachary@gmail.com>
2022-01-22 09:50:38 -0500

    licensing info

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# ReadMe-Generator
+# Readme Generator
+
+![badmath](https://img.shields.io/badge/License-MIT-<green>)
+## Description 
+
+
+This project utilizes node.js to create readme files by prompting users for information about their projects.  This ReadMe was created using the app.
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+Install Node.js on your computer following the instructions here https://nodejs.org/en/download/.  Then copy the files to your computer.  Start the program using the command line in the root directory.  The starting command is node index.
+
+## Usage 
+The repo is available to be utilized by anyone.
+
+## Contributing
+Feel free to download or fork from the orginal and make your own changes.
+
+## Tests
+There are no testing instructions for this app.
+
+## Questions
+https://github.com/Zsvoboda87
+
+Email Me @ svobodazachary@gmail.com
+
+## License
+MIT License
+    
+Copyright (c) 2022 Zachary Svoboda
+      
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+      
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+      
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- index.js
+++ index.js
@@ -4,130 +4,143 @@ const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // Function to prompt and collect User input for the ReadMe
 const questions = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username:',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter your GitHub Username!');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter your email address:',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter your email address!');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of the project?',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter a project name!');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter a description of the project:',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter a project description');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What license does this project have',
-            choices: ['MIT', 'APACHE_2.0', 'GPL_3.0', 'BSD3', 'None']
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Enter the installation instructions:',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter project installation requirements');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'Enter testing instructions:',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter a testing instructions');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'userInfo',
-            message: 'Enter information about using the Repo:',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter user instructions for Repo or "none"');
-                  return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'userInfo',
-            message: 'Enter information about using the Repo:',
-            validate: nameInput => {
-                if (nameInput) {
-                  return true;
-                } else {
-                  console.log('Please enter user instructions for Repo or "none"');
-                  return false;
-                }
-            }
-        },
-        {
-          type: 'input',
-          name: 'contribute',
-          message: 'Enter information about contributing to the Repo:',
-          validate: nameInput => {
-              if (nameInput) {
-                return true;
-              } else {
-                console.log('Please enter contributing instructions');
-                return false;
-              }
-          }
-      },
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub Username!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your email address!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the project?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter a project name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of the project:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter a project description');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license does this project have',
+      choices: ['MIT', 'APACHE_2.0', 'GPL_3.0', 'ISC', 'None']
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter the installation instructions:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter project installation requirements');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Enter testing instructions:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter a testing instructions');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'userInfo',
+      message: 'Enter information about using the Repo:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter user instructions for Repo or "none"');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'userInfo',
+      message: 'Enter information about using the Repo:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter user instructions for Repo or "none"');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'contribute',
+      message: 'Enter information about contributing to the Repo:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter contributing instructions');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter Full Name for Licensing:',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter Full Name');
+          return false;
+        }
+      }
+    },
          
     ]);
 };
